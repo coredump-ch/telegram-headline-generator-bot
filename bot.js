788a3e5a491d5e507b343600c9879b9791cb8e36
Jannis Grimm <jannis@gje.ch>
2015-11-25 19:55:47 +0100

    Tune generator values

--- bot.js
+++ bot.js
@@ -78,9 +78,9 @@ function getHeadlineTokens(history, hadPart2) {
 	
 	var randomizer = [];
 	randomizer = randomizer.concat(getFollowerCollection(currentFollowers, 0, 1, true));
-	randomizer = randomizer.concat(getFollowerCollection(currentFollowers, 1, 3, false));
-	randomizer = randomizer.concat(getFollowerCollection(currentFollowers, 2, 3, false));
-	randomizer = randomizer.concat(getFollowerCollection(currentFollowers, 3, 2, false));
+	randomizer = randomizer.concat(getFollowerCollection(currentFollowers, 1, 2.5, false));
+	randomizer = randomizer.concat(getFollowerCollection(currentFollowers, 2, 2.5, false));
+	randomizer = randomizer.concat(getFollowerCollection(currentFollowers, 3, 1.5, false));
 
 	while (true) {
 		if (!randomizer.length) {
