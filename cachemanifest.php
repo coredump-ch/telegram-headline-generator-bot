729d284c6ba72a815e4e7134584293d8436e620a
Jannis Grimm <jannis@gje.ch>
2016-01-20 21:43:05 +0100

    Squashed 'headlinegenerator/' content from commit ca7401d
    git-subtree-dir: headlinegenerator
    git-subtree-split: ca7401dd9dae6a5b2c0961889acbd7044061b95c

---
+++ cachemanifest.php
@@ -0,0 +1,14 @@
+<?php
+	header('Content-Type: text/cache-manifest; charset=utf-8');
+?>
+CACHE MANIFEST
+
+# Generator v2.1
+# Headlines v24
+
+index.php
+scripts.js
+generator_functions.js
+followers.json
+headlines.json
+
