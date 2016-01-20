22ba50990586e32a1bb559e7f453dc90489a2cb5
Jannis Grimm <jannis@gje.ch>
2016-01-20 21:51:05 +0100

    Mention /generate on /start

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var bot = new TelegramBot(config.token, {polling: true});
 bot.onText(/^\/start(@HeadlineGeneratorBot)?$/, function(message, match) {
   var messageId = message.message_id;
   var chatId = message.chat.id;
-  bot.sendMessage(chatId, 'Hey!', {'reply_to_message_id': messageId});
+  bot.sendMessage(chatId, 'Hey!\n\nNutze /generate, um eine Schlagzeile zu generieren.', {'reply_to_message_id': messageId});
 });
 
 bot.onText(/^\/help(@HeadlineGeneratorBot)?$/, function(message, match) {
